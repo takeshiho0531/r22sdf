d132df13e2043e2bce1a26eb1d67ffed0c8c1086
takeshiho0531 <kawadatakeshiho@gmail.com>
2023-12-25 10:52:17 +0900

    wip: debug log N=64; twiddle addr

--- r22sdf64/r22sdf64.srcs/sources_1/imports/verilog/SdfUnit.v
+++ r22sdf64/r22sdf64.srcs/sources_1/imports/verilog/SdfUnit.v
@@ -281,9 +281,10 @@ assign  do_im = (LOG_M == 2) ? bf2_do_im : mu_do_im;
 
 always @(posedge clock) begin
     // $display("di_count=%d, bf1_start=%d, bf1_sp_en=%d, bf1_count=%d, bf1_end=%d, bf2_start=%d, bf2_sp_en=%d, bf2_count=%d, bf2_end=%d", di_count, bf1_start, bf1_sp_en, bf1_count, bf1_end, bf2_start, bf2_sp_en, bf2_count, bf2_end);
-    if (bf2_x0_re>=0 | bf2_x0_re<0) begin
-        $display("bf2_bf=%d, bf2_x0_re=%d, bf2_x0_im=%d, bf2_x1_re=%d, bf2_x1_im=%d, bf2_y0_re=%d, bf2_y0_im=%d, bf2_y1_re=%d, bf2_y1_im=%d", bf2_bf, bf2_x0_re, bf2_x0_im, bf2_x1_re, bf2_x1_im, bf2_y0_re, bf2_y0_im, bf2_y1_re, bf2_y1_im);
-    end
+    // if (bf2_x0_re>=0 | bf2_x0_re<0) begin
+    //     $display("bf2_bf=%d, bf2_x0_re=%d, bf2_x0_im=%d, bf2_x1_re=%d, bf2_x1_im=%d, bf2_y0_re=%d, bf2_y0_im=%d, bf2_y1_re=%d, bf2_y1_im=%d", bf2_bf, bf2_x0_re, bf2_x0_im, bf2_x1_re, bf2_x1_im, bf2_y0_re, bf2_y0_im, bf2_y1_re, bf2_y1_im);
+    // end
+            $display("LOG_M=%d, di_count=%d, bf1_bf=%d, bf2_bf=%d, bf1_count=%d, tw_sel=%b, tw_num=%d, tw_addr=%d, mu_en=%b", LOG_M, di_count, bf1_bf, bf2_bf, bf1_count, tw_sel, tw_num, tw_addr, mu_en);
 end
 
 endmodule
